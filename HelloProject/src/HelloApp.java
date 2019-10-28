72f30e43fef96aa5f66bfb729695d8b045635189
gthebeauUNB <57004998+gthebeauUNB@users.noreply.github.com>
2019-10-28 15:24:22 -0300

    Update HelloApp.java
    changed to print out my full name

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Greg!");
+		System.out.println("Hello Gregory Adam Thebeau!");
 	}
 
 }
